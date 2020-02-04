cc77479ad997d4829fdddc041f00de699f288f5e
capntechbeard <capntechbeard@gmail.com>
2020-02-03 21:23:52 -0600

    first push to github
    Includes: Noita, Hydromancer, Pyromancer, & Electromancer.

---
+++ loadouts_expanded/files/loadouts.lua
@@ -0,0 +1,79 @@
+loadout_list =
+{
+
+	--[[
+	{
+		name = "the Noita",
+		folder = "noita",
+		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
+		cape_color = 0xff8f6790,
+		cape_color_edge = 0xff6d4e76,
+		items = 
+		{
+			"mods/loadouts_expanded/files/loadouts/noita/wands/wand_1.xml",
+			"mods/loadouts_expanded/files/loadouts/noita/wands/wand_2.xml",
+			"data/entities/items/pickup/potion_water.xml",
+		},
+		perks =
+		{
+			"EDIT_WANDS_EVERYWHERE",
+		},
+	},
+	{
+		name = "a Hydromancer",
+		folder = "hydromancer",
+		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
+		cape_color = 0xffa9782c,
+		cape_color_edge = 0xff544221,
+		items = 
+		{
+			"mods/loadouts_expanded/files/loadouts/hydromancer/wands/wand_1.xml",
+			"mods/loadouts_expanded/files/loadouts/hydromancer/wands/wand_2.xml",
+			{
+			"data/entities/items/pickup/potion_water.xml",
+			count = 4,
+			}
+		},
+		perks =
+		{
+			"BREATH_UNDERWATER",
+			"SPEED_DIVER",
+		},
+	},
+	{
+		name = "a Pyromancer",
+		folder = "pyromancer",
+		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
+		cape_color = 0xff2b3cb0,
+		cape_color_edge = 0xff1b3776,
+		items = 
+		{
+			"mods/loadouts_expanded/files/loadouts/pyromancer/wands/wand_1.xml",
+			"mods/loadouts_expanded/files/loadouts/pyromancer/wands/wand_2.xml",
+			"mods/loadouts_expanded/files/potions/potion_fire.xml",
+		},
+		perks =
+		{
+			"PROTECTION_FIRE",
+		},
+	},
+	]]--
+		{
+		name = "an Electromancer",
+		folder = "electromancer",
+		-- NOTE: Usually the game uses ARGB format for colours, but the cape colour uses ABGR here instead
+		cape_color = 0xffcecfb8,
+		cape_color_edge = 0xffb6b492,
+		items = 
+		{
+			"mods/loadouts_expanded/files/loadouts/electromancer/wands/wand_1.xml",
+			"mods/loadouts_expanded/files/loadouts/electromancer/wands/wand_2.xml",
+			"data/entities/items/pickup/thunderstone.xml",
+		},
+		perks =
+		{
+			"PROTECTION_ELECTRICITY",
+		},
+	},
+
+}
